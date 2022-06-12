5568cb24901c13ec0e93a15ebfa0fd38c5537376
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 19:25:57 -0700

    add function for intern class

--- js/intern.js
+++ js/intern.js
@@ -4,3 +4,23 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 // add to employee.js
 var EmployeeRank = require('./employee');
+
+// create class Intern extentsion of employee
+class Intern extends EmployeeRank {
+    // create constructor
+    constructor(name, id, email, school){
+        super(name, id, email);
+        // add school
+        this.school = school;
+        // add role
+        this.role = "Intern"
+    }
+    // create funciton for school
+    getSchool () {
+        return `My school is ${this.school}`;
+    }
+    // create function for role
+    getRole() {
+        return `My role is ${this.role}`;
+    }
+}
