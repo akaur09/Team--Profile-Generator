a02d35597f5573b99c92c145866a887160981fba
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 18:41:47 -0700

    create constructor

--- js/engineer.js
+++ js/engineer.js
@@ -4,3 +4,14 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 // call on employee.js
 var EmployeeRank = require('./employee');
+
+// create engineer class which is an extentsion of employee.js
+class Engineer extends EmployeeRank {
+    // add constructor to create name, id, email and github
+    constructor (name, id, email, gitUsername){
+        super(name, id, email);
+        this.gitUsername = gitUsername;
+        // create role
+        this.role = "Engineer";
+    }
+}
