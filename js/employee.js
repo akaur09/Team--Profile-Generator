159dabebc37d4e70b8f31ad3fa30652a5c33be9b
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 18:30:26 -0700

    assign role and id, name and email

--- js/employee.js
+++ js/employee.js
@@ -2,6 +2,11 @@
 class Employee{
     // add constructor for name, id and email
     constructor(name, id, email) {
-        
+        // assign id, name and email
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        // assign role
+        this.role = "Employee";
     }
 }
