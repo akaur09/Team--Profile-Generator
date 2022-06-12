0367ee8a9999912dfbeea98c6fef309e30e781f9
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 23:52:19 -0700

    add tests for email, id, name and role

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -20,4 +20,27 @@ describe('Employee', () => {
         })
     })
     // get name test
+    describe('function getName', ()=> {
+        it("returns name of employee object", () => {
+            expect(janeDoe.getName()).toEqual("this is jane doe")
+        })
+    })
+    // getId test
+    describe('function getId', ()=> {
+        it("returns Id of employee", () => {
+            expect(janeDoe.getId()).toEqual("my id is 9")
+        })
+    })
+    // getEmail test
+    describe('function getEmail', () => {
+        it("returns employees email", () => {
+            expect(janeDoe.getEmail()).toEqual("my email is JaneDoe@gmail.com")
+        })
+    })
+    // getRole funciton test
+    describe('function getRole', () => {
+        it("returns employee role", () => {
+            expect(janeDoe.getRole()).toEqual("my role is employee")
+        })
+    })
 })
