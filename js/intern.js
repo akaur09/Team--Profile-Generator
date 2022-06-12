321eda2f5411035ebecd5fe7f0a7e530c09a5cab
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 19:33:29 -0700

    add function for name and id

--- js/intern.js
+++ js/intern.js
@@ -24,3 +24,35 @@ class Intern extends EmployeeRank {
         return `My role is ${this.role}`;
     }
 }
+// create function for inputting intern
+function inquireIntern (){
+    return inquirer.prompt([
+        {
+            // input name
+            type:"input",
+            message: "Enter INTERN name: ",
+            name: "inputName",
+            validate: (inputvalue) => {
+                if (inputvalue) {
+                    return true;
+                } else {
+                    console.log("Please enter a vaild name: ");
+                    return false;
+                }
+            }
+        },
+        {
+            // input id
+            type:"input",
+            message: "enter INTERN Id: ",
+            validate: (inputvalue) => {
+                if (inputvalue) {
+                    return true;
+                } else {
+                    console.log("Please enter a vaild id: ");
+                    return false;
+                }
+            }
+        }
+    ])
+}
