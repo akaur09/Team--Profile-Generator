0b38889cf8d29ddeb5cc4b576a01eb6b462a54fc
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 19:44:15 -0700

    add function to generate html

--- js/intern.js
+++ js/intern.js
@@ -88,3 +88,19 @@ function inquireIntern (){
         return InternHTML(newIntern);
     })
 }
+// create funciton to generate intern html
+function InternHTML(data) {
+    var internString= `
+    <div>
+        <div> ${data.role} </div>
+        <div>
+            <h4> ${data.name} </h4>
+            <p> ${data.id} </p>
+            <p> Email: <a href = "mailto:${data.email}"> ${data.email} </a></p>
+            <p> School: ${data.school} </p>
+        </div>
+    </div>
+    `
+    return internString;
+}
+module.exports = (inquireIntern, Intern);
