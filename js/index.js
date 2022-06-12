9ddfeb248a21b04677c8e731032cdf50e1f09aad
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 23:15:04 -0700

    add html

--- js/index.js
+++ js/index.js
@@ -62,5 +62,30 @@ function addEmployee(){
 }
 // create an HTML with the complied HTMLs
 function compileHTML() {
-    var fullString = ``
+    var fullString = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <scriptsrc="https://kit.fontawesome.com/30f3a925af.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Member Generator</title>
+    </head>
+    <body>
+        <main>
+            <div class="heading">
+                <h1> Team Members</h1>
+            </div>
+            <div id="flexThis">
+                ${HTMLString}
+            </div>
+        </main>
+        <script src="./js/index.js"></script>
+    </body>
+    </html>
+    `;
 }
