b724bd6f4ba3cfe639d14ac93abd86ad18d54adf
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 19:11:05 -0700

    create html

--- js/engineer.js
+++ js/engineer.js
@@ -94,3 +94,21 @@ function inquireEngineer () {
         return EngineerHTML(newEngineer);
     })
 }
+// create a function to generate engineer html
+function EngineerHTML(data) {
+    var EngineerString = 
+    `
+    <div>
+        <div> ${data.role} </div>
+        <div>
+            <h4> ${data.name} </h4>
+            <p> ${data.id} </p>
+            <p> Email: <a href = "mailto:${data.email}"> ${data.email} </a></p>
+            <p> Github username: <a href = "https://github.com/${data.gitUsername}/" target= "_blank"> Github </a></p>
+        </div>
+    </div>
+    `
+    return EngineerString;
+}
+
+module.exports = {inquireEngineer, Engineer};
