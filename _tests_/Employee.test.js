c6b294f4829a0513800c10cffa91a9d762b83d3b
akaur091 <amniderkaur.09@gmail.com>
2022-06-12 00:29:39 -0700

    fix test text

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -10,7 +10,7 @@ describe('Employee', () => {
     // call on name id email into employee object
     janeDoe = new ployee(name, id, email);
     it("Makes new employee object", () => {
-        expect(typeof janeDoe).toEqual(`objecct`);
+        expect(typeof janeDoe).toEqual(`object`);
     })
 
     // test if employee exists
@@ -22,25 +22,25 @@ describe('Employee', () => {
     // get name test
     describe('function getName', ()=> {
         it("returns name of employee object", () => {
-            expect(janeDoe.getName()).toEqual("this is jane doe")
+            expect(janeDoe.getName()).toEqual("This is JaneDoe")
         })
     })
     // getId test
     describe('function getId', ()=> {
         it("returns Id of employee", () => {
-            expect(janeDoe.getId()).toEqual("my id is 9")
+            expect(janeDoe.getId()).toEqual("My eployee ID is 9")
         })
     })
     // getEmail test
     describe('function getEmail', () => {
         it("returns employees email", () => {
-            expect(janeDoe.getEmail()).toEqual("my email is JaneDoe@gmail.com")
+            expect(janeDoe.getEmail()).toEqual("My work email is JaneDoe@gmail.com")
         })
     })
     // getRole funciton test
     describe('function getRole', () => {
         it("returns employee role", () => {
-            expect(janeDoe.getRole()).toEqual("my role is employee")
+            expect(janeDoe.getRole()).toEqual("My role here is Employee")
         })
     })
 })
