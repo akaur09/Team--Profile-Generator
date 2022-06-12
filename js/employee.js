b1701932601244a0b2e14777c2a1f31911b021dd
akaur091 <amniderkaur.09@gmail.com>
2022-06-11 18:34:52 -0700

    add functions to call on constructors

--- js/employee.js
+++ js/employee.js
@@ -9,4 +9,23 @@ class Employee{
         // assign role
         this.role = "Employee";
     }
+    // create a function to get name
+    getName(){
+        return `This is ${this.name}`;
+    }
+    // create a function to get ID
+    getId(){
+        return `My employee ID is ${this.id}`;
+    }
+    // create function to get email
+    getEmail() {
+        return `My work email is ${this
+        .email}`;
+    }
+    // create funciton to return role of worker
+    getRole(){
+        return `My role here is ${this.role}`;
+    }
 }
+
+module.exports = Employee;
