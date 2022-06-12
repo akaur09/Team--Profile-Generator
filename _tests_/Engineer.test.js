afc312adef7b54fe30e418d5bdb78172c17d8792
akaur091 <amniderkaur.09@gmail.com>
2022-06-12 00:03:32 -0700

    finish adding test for eachfunction

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -13,4 +13,39 @@ describe('Engineer subclass', () => {
         expect(typeof janeDoe).toEqual(`object`);
     })
     // if engineer with same name exist
+    describe('JaneDoe Engineer exists', () => {
+        it('confirms that JaneDoe is an instance of engineer class', () => {
+            expect(janeDoe).toBeDefined();
+        })
+    })
+     // get name test
+     describe('function getName', ()=> {
+        it("returns name of employee object", () => {
+            expect(janeDoe.getName()).toEqual("this is jane doe")
+        })
+    })
+    // getId test
+    describe('function getId', ()=> {
+        it("returns Id of employee", () => {
+            expect(janeDoe.getId()).toEqual("my id is 9")
+        })
+    })
+    // getEmail test
+    describe('function getEmail', () => {
+        it("returns employees email", () => {
+            expect(janeDoe.getEmail()).toEqual("my email is JaneDoe@gmail.com")
+        })
+    })
+    // getGithub function test
+    describe('function getGtihub', () => {
+        it("returns engineers github", () => {
+            expect(janeDoe.getGithub()).toEqual("my github is JDcode9")
+        })
+    })
+    // getRole funciton test
+    describe('function getRole', () => {
+        it("returns employee role", () => {
+            expect(janeDoe.getRole()).toEqual("my role is employee")
+        })
+    })
 })
